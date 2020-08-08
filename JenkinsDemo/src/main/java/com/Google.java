55176cac711a601f09bdadda0140cda4eecd73a7
Vinaythebeginner <vinaykumar.gunti3@gmail.com>
2020-08-09 05:13:11 +0530

    logging

--- JenkinsDemo/src/main/java/com/Google.java
+++ JenkinsDemo/src/main/java/com/Google.java
@@ -7,10 +7,11 @@ import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 //http://seleniumhq.github.io/selenium/docs/api/java/index.html-JAVADOC
 public class Google {
-
-	public static void main(String[] args) {
+@Test
+	public void test() {
 		// TODO Auto-generated method stub
 
 		
