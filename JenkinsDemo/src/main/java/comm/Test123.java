7cd1c790d81c0e98a8e27ba88bae6fc9afe2698a
Vinaythebeginner <vinaykumar.gunti3@gmail.com>
2020-08-09 03:26:45 +0530

    Committed first change

---
+++ JenkinsDemo/src/main/java/comm/Test123.java
@@ -0,0 +1,22 @@
+package comm;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test123 {
+
+	WebDriver driver;
+	
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Coding\\Seleniumlib\\ChromeDriver\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		Assert.assertTrue(driver.getTitle().equalsIgnoreCase("Google"));
+		driver.quit();
+	}
+}
